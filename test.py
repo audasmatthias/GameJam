bd02c6bedd7e407db022f64643a793a4f3cca95b
audasmatthias <50580378+audasmatthias@users.noreply.github.com>
2020-02-03 11:41:43 +0100

    Boucle de jeu

--- test.py
+++ test.py
@@ -1,1 +1,16 @@
-print("Hello world")
+import pygame
+pygame.init()
+
+win = pygame.display.set_mode((640.480))
+
+pygame.display.set_caption("GameJam")
+
+run = True
+while run:
+    pygame.time.delay(100)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run=False
+
+pygame.quit()            
