07839190c769e67ad2f397b46f9bbc4bc87d6891
audasmatthias <50580378+audasmatthias@users.noreply.github.com>
2020-02-03 12:05:51 +0100

    test

--- test.py
+++ test.py
@@ -1,26 +1,14 @@
 import pygame
 pygame.init()
 
-<<<<<<< HEAD
 win = pygame.display.set_mode((640, 480))
-=======
-win = pygame.display.set_mode((640.480))
->>>>>>> bd02c6bedd7e407db022f64643a793a4f3cca95b
-
 pygame.display.set_caption("GameJam")
 
 run = True
 while run:
     pygame.time.delay(100)
-
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-<<<<<<< HEAD
             run = False
 
 pygame.quit()
-=======
-            run=False
-
-pygame.quit()            
->>>>>>> bd02c6bedd7e407db022f64643a793a4f3cca95b
